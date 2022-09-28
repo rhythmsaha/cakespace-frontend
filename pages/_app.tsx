c4fdb3c1128051312f66c05796fcc0f94aadc1f9
Rhythm Saha <rs.2001.saha@gmail.com>
2022-09-28 19:13:40 +0530

    nprogress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,27 @@ import "../styles/globals.css";
 import type { ReactElement, ReactNode } from "react";
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
+import { Router } from "next/router";
 import { Provider } from "react-redux";
 import { store } from "../store";
 import AuthProvider from "../components/layouts/AuthProvider";
 import { Toaster } from "react-hot-toast";
+import NProgress from "nprogress";
+
+// progress bar
+NProgress.configure({ showSpinner: false });
+
+Router.events.on("routeChangeStart", () => {
+  NProgress.start();
+});
+
+Router.events.on("routeChangeComplete", () => {
+  NProgress.done();
+});
+
+Router.events.on("routeChangeError", () => {
+  NProgress.done();
+});
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -29,3 +46,5 @@ export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
     </>
   );
 }
+
+NProgress.configure({ showSpinner: false });
